37a8065b4b9d519c7c40908159a7096b3e475932
Park Hyung Seo <sonbbal20@gmail.com>
2020-11-24 18:49:50 +0900

    Add week component to conrol pannel

--- src/Components/ControlPannel/index.js
+++ src/Components/ControlPannel/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../../css/controlPannel.scss';
 import Month from 'Components/ControlPannel/Month';
+import Week from './Week';
 
 const ControlPannel = () => {
     return (
         <div className="control-pannel">
             <h2>컨트롤 패널</h2>
             <Month />
+            <Week />
         </div>
     )
 }
